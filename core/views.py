4c92ebdcb0676b0f3799a6284302f130a70583cb
Gatoxic0 <re.romanini@duocuc.cl>
2024-06-06 11:53:50 -0400

    Agregar productos desde BD al menu

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import *
 from django.contrib.auth.views import logout_then_login
+from .models import *
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html',)
 
 def logout(request):
     return logout_then_login(request, login_url="login")
@@ -18,8 +19,8 @@ def header(request):
     return render(request, 'header.html')
 
 def menu(request):
-    productos = Producto.objects.all()
-    return render(request, 'menu.html', {'productos':productos})
+    tortas = Producto.objects.all()
+    return render(request, 'menu.html', {'tortas':tortas})
 
 def pedidos(request):
     return render(request, 'pedidos.html')

73d4318c52b382e468e4ceb1e2e6d1fdb8629eeb
caritomiserias <caro.avila@duocuc.cl>
2024-07-01 19:51:05 -0400

    estilo a login y registro

--- core/forms.py
+++ core/forms.py
@@ -4,7 +4,18 @@ from django.contrib.auth.models import User
 
 class RegistroForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Ingrese un correo electrónico válido.')
-
+    
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        labels = {
+            'username': 'Nombre de usuario',
+            'email': 'Email',
+            'password1': 'Contraseña',
+            'password2': 'Confirmar contraseña',
+        }
+        help_texts = {
+            'username': 'Mínimo 6 caracteres. Únicamente letras.',
+            'password1': 'Su contraseña no puede asemejarse tanto a su otra información personal. Debe contener al menos 8 caracteres. No puede ser una clave utilizada comúnmente. No puede ser completamente numérica.',
+            'password2': 'Para verificar, introduzca la misma contraseña anterior.',
+        }
